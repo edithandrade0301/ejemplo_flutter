67989d6d4fcca45c765cc9cf093777d9823a0965
edithandrade0301 <edithyamilethandrade03@gmail.com>
2025-01-31 18:38:29 -0600

    PRIMERO CAMBIO
    se cambio un titulo

--- lib/main.dart
+++ lib/main.dart
@@ -106,7 +106,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'You have pushed the button this many times:',
+              'ESTO ES UNA PRUEBA:',
             ),
             Text(
               '$_counter',
